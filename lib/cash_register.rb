27e66ad440fe8125835a58774aa3b2fe9693da2f
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-25 14:21:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class CashRegister
 
   attr_accessor :total, :discount, :items
@@ -27,7 +26,6 @@ class CashRegister
   end
 
   def void_last_transaction
-    binding.pry
     last_item = items.length
     self.total - last_item.price
 
